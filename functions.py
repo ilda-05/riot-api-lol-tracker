234cf04bdfa59ffe6552a8509e17022d4540bb7f
ilda-05 <191975905+ilda-05@users.noreply.github.com>
2026-04-10 15:03:40 +0200

    added caching for players name

--- functions.py
+++ functions.py
@@ -20,7 +20,7 @@ def searchPlayer(playerName, playerTag):
         print("Player not found")
 
 
-# Search the id of last 10 games of the player
+# Returns the id of last 10 games of the player
 def searchPlayerMatches(puuid:Player):
     
     req = requests.get("https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{playerPuuid}/ids?start=0&count=10&api_key={api}".format(playerPuuid=puuid,api=API_KEY))
--- main.py
+++ main.py
@@ -2,25 +2,17 @@ from functions import *
 import os
 
 def clr():
+
     os.system('cls' if os.name == 'nt' else 'clear')
 
-###
-# 
-# 0 Exit
-#
-# 1 Search Player
-#
-# 2 Champions Rotation
-#  
-###
+player_cache=dict()
 
 def main():
 
+    clr()
     checksApiKey()  
-    
+        
     while(True):
-
-
         
         # Menu'
 
@@ -31,18 +23,43 @@ def main():
         clr()
         
         if user_input == "0":
+
             break
+
         elif user_input == "1":
+
             playerName=input("Player Name : ")
             playerTag=input("Player Tag : ")
-            clr()
             print("Loading...")
-            player = searchPlayer(playerName,playerTag)
-            games = searchPlayerMatches(player.getPuuid())
-            clr()
-            matchesInfo(games,player)
+            
+            if playerName+"#"+playerTag in player_cache:
+
+                player = player_cache[playerName+"#"+playerTag]
+
+            else:
+
+                player = searchPlayer(playerName,playerTag)
+                player_cache.update({playerName+"#"+playerTag:player})
+                print(player)
+
+            if player != None:
+
+                games = searchPlayerMatches(player.getPuuid())
+
+                matchesInfo(games,player)
+
+            else:
+
+                print("Player not found")
+
+        elif user_input == "2":
+
+            print(player_cache)
+
         else:
+
             continue
 
 if __name__ == "__main__":
+
     main()
