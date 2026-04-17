8f6948de398d5a2b8daab88c0388692612e3e27b
ilda-05 <191975905+ilda-05@users.noreply.github.com>
2026-04-17 17:19:09 +0200

    .

--- functions.py
+++ functions.py
@@ -1,13 +1,16 @@
 import requests
 from api_key import API_KEY
 from Player import Player
+from Match import Match
 
 def checksApiKey():
     if(API_KEY==None):
         print("Api key missing")
         exit()
 
-#Checks if the player exists and then return the Object Player
+# Checks if the player exists
+# then return the Object Player
+# Return type : Player
 def searchPlayer(playerName, playerTag):
     req = requests.get("https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{name}/{tag}?api_key={api}".format(name=playerName,tag=playerTag,api=API_KEY))
     if(req.status_code==200):
@@ -21,29 +24,38 @@ def searchPlayer(playerName, playerTag):
 
 
 # Returns the id of last 10 games of the player
+# Return type : list
 def searchPlayerMatches(puuid:Player):
     
     req = requests.get("https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{playerPuuid}/ids?start=0&count=10&api_key={api}".format(playerPuuid=puuid,api=API_KEY))
-    idMatches = req.json()
+    idMatches:list = req.json()
+    print(type(idMatches))
     return idMatches
 
-# Gets the details of a list of games
-def matchesInfo(games,player):
+# Return the details of the game
+# Return type : list
+def matchesInfo(list_matchId:list):
 
     stat_games = []
+    matchId = str()
 
-    for game in games:
+    for game in list_matchId:
         req = requests.get("https://europe.api.riotgames.com/lol/match/v5/matches/{idGame}?api_key={api}".format(idGame=game,api=API_KEY))
         content = req.json()
+
+        metadata=content["metadata"]
+
+        matchId=metadata["matchId"]
+
         info_game=content["info"]
         info_participants = info_game["participants"]
+
         for participant in info_participants:
             if participant["puuid"] == player.getPuuid():
                 stat_games.append("Stats : {champ} {kills}/{deaths}/{assists}".format(kills=participant["kills"],deaths=participant["deaths"],assists=participant["assists"], champ=participant["championName"]))
                 break
 
-    for game in stat_games :
-        print(game)
+    return stat_games
     
             
 
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def clr():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 player_cache=dict()
+match_cache=list()
+match_details_cache=dict()
+
 
 def main():
 
@@ -16,20 +19,27 @@ def main():
         
         # Menu'
 
+        print("\n--------------------")
         print("0 - Exit")
         print("1 - Search Player")
+        print("--------------------\n")
+
         user_input = input("Choose : ")
 
         clr()
         
+        # Stops the execution
         if user_input == "0":
 
             break
 
+        # Searches for a player details
         elif user_input == "1":
 
+            
             playerName=input("Player Name : ")
             playerTag=input("Player Tag : ")
+
             print("Loading...")
             
             if playerName+"#"+playerTag in player_cache:
@@ -45,19 +55,31 @@ def main():
             if player != None:
 
                 games = searchPlayerMatches(player.getPuuid())
+                games_details=list()
+                
+                for game in games:
+                    if game not in match_cache:
+                        match_cache.append(game)
 
-                matchesInfo(games,player)
+                print(match_cache)
+                games_details=matchesInfo(games,player)
+                for game_details in games_details:
+                    match_details_cache.update({game:game_details})
+                    print(game)
 
             else:
-
                 print("Player not found")
 
         elif user_input == "2":
-
             print(player_cache)
+        
+        elif user_input == "3":
+            print(match_cache)
+
+        elif user_input == "4":
+            print(match_details_cache)
 
         else:
-
             continue
 
 if __name__ == "__main__":
